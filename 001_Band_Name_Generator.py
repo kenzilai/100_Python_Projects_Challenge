10a951ab8e005f6e6458fbc4445ccc8e4de060fc
Kenzi <105054549+kenzilai@users.noreply.github.com>
2022-12-09 03:36:21 -0800

    Band Name Generator

---
+++ 001_Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("Your brand name could be " + city_name + " " + pet_name)
